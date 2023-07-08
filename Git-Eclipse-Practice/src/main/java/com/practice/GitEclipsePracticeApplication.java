10594d3589444991dc4b967650366166ea5bf118
Prateek Gupta <prateekgupta9682@gmail.com>
2023-07-08 15:51:37 +0530

    First Commit from eclipse

---
+++ Git-Eclipse-Practice/src/main/java/com/practice/GitEclipsePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEclipsePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEclipsePracticeApplication.class, args);
+	}
+
+}
